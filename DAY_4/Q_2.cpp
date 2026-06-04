3af68c66ebdb386a8229905f5738e30290c2b197
AsthaVerma-2029 <asthaverma2029@gmail.com>
2026-06-04 16:57:36 +0530

    DAY_4 Assignment uploaded

--- DAY_4/Q_2.cpp
+++ DAY_4/Q_2.cpp
@@ -1,19 +1,20 @@
-// WRITE A PROGRAM TO FIND NTH FIBONACCI TERM 
-
+// WRITE A PROGRAM TO FIND NTH FIBONACCI TERM
 
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n1=0,n2=1,n,temp;
-    cout<<"Enter the term of fibonacci : ";
-    cin>>n;
-    cout<<"the "<<n<<"th term of the fibonacci is : ";
-    for(int i=0;i<n;i++){
-        temp=n1+n2;
-        n1=n2;
-        n2=temp;
+int main()
+{
+    int n1 = 0, n2 = 1, n, temp;
+    cout << "Enter the term of fibonacci : ";
+    cin >> n;
+    cout << "the " << n << "th term of the fibonacci is : ";
+    for (int i = 0; i < n; i++)
+    {
+        temp = n1 + n2;
+        n1 = n2;
+        n2 = temp;
     }
-    cout<<n1;
-return 0;
+    cout << n1;
+    return 0;
 }
--- DAY_4/Q_3.cpp
+++ DAY_4/Q_3.cpp
@@ -1,36 +1,41 @@
 // WRITE A PROGRAM TO CHECK ARMSTRONG NUMBER
 
 #include <iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
 
-int digit_count(int n){
-    int count=0;
-    while(n!=0){
-        n=n/10;
-        count+=1;
+int digit_count(int n)             //function to count digits
+{
+    int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        count += 1;
     }
     return count;
 }
 
-int main(){
-    int n,x,digit,sum=0,n1;
-    cout<<"Enter the number to be checked : ";
-    cin>>n;
-    x=digit_count(n);
-    n1=n;
-    while(n!=0){
-        digit=n%10;
-        n=n/10;
-        sum=sum+pow(digit,x);
+int main()
+{
+    int n, x, digit, sum = 0, n1;
+    cout << "Enter the number to be checked : ";
+    cin >> n;
+    x = digit_count(n);
+    n1 = n;
+    while (n != 0)
+    {
+        digit = n % 10;
+        n = n / 10;
+        sum = sum + pow(digit, x);       //sum of each digit raised to power of the number of digits
+    }
+    if (sum == n1)
+    {
+        cout << "The number is armstrong number ";               
+    }
+    else
+    {
+        cout << "The number is not armstrong number ";
+    }
 
-    }
-    if(sum==n1){
-        cout<<"The number is armstrong number ";
-    }
-    else{
-        cout<<"The number is not armstrong number ";
-    }
-
-return 0;
+    return 0;
 }
